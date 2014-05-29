ca5539e3b35e73d35394f01a790d0e829ec5d01d
Luís Resende <luisresende@MacBook-Pro-de-Luis.local>
2014-05-29 15:48:29 -0300

    Initial Commit

---
+++ ProjetoQQISSO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjetoQQISSO
+//
+//  Created by Luís Resende on 29/05/14.
+//  Copyright (c) 2014 Luis Resende. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProjetoQQISSOTests/ProjetoQQISSOTests.m
@@ -0,0 +1,34 @@
+//
+//  ProjetoQQISSOTests.m
+//  ProjetoQQISSOTests
+//
+//  Created by Luís Resende on 29/05/14.
+//  Copyright (c) 2014 Luis Resende. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProjetoQQISSOTests : XCTestCase
+
+@end
+
+@implementation ProjetoQQISSOTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
